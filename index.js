1f1f128979ce5dfd99ef39a6d0f474a308b58a5e
Faisal Ahmed <git@fais.al>
2024-07-23 13:04:17 +0200

    #1 used middleware instead so that I can see what was accessed even if route doesn't exist

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+  next();
+});
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
-    console.log(`${req.method} ${req.originalUrl}`);
+app.get('/', (req, res) => {
+  res.send('Hello World');
 })
 
 app.listen(port, ()=> {
-    console.log('Server up and running');
+  console.log('Server up and running');
 })
 
 
