7c1001be3360ea15cfa141be9dcaa5bb2cda0fef
Faisal Ahmed <git@fais.al>
2024-07-23 13:23:42 +0200

    #2 Installed Helmet and Disabeld X-Powered-By Express header field

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.disable("x-powered-by");
+
+app.use(helmet());
 
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.url}`);
@@ -10,11 +15,11 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-})
+});
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log('Server up and running');
-})
+});
 
 
 
