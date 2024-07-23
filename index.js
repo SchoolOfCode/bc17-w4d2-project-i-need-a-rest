221c24c2207efd8571cb81c3be82c4f1f909095d
Faisal Ahmed <git@fais.al>
2024-07-23 12:51:32 +0200

    #1 Console.log done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express'
+import express from 'express';
 
-const app= express()
-const port=3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.send('Hello World');
+    console.log(`${req.method} ${req.originalUrl}`);
 })
 
-
 app.listen(port, ()=> {
-    console.log('Server up and running')
+    console.log('Server up and running');
 })
 
 
